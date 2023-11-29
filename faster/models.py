7ef55fe8de3fe9965de8b25af7b9df55ff47bc20
BrightNetTech <masikabright@gmail.com>
2023-11-29 15:16:34 +0300

    First commit

---
+++ faster/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    Name = models.CharField(max_length=50)
+    Email = models.EmailField()
+    Subject = models.CharField(max_length=100)
+    Message = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.Name
+
+class Track(models.Model):
+    Map = models.Geolocation()
+    Longitude = models.GPRS()
+    Latidude = models.GPRS()
+    Tracking_id = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.Map
+
+
+    
+
